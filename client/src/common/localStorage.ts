67b853ca6d0acb5cc880c32374f47e6427752eae
David Grankvist <grankvist.david@gmail.com>
2022-10-25 20:07:22 +0200

    prefill lobby form

--- client/src/common/localStorage.ts
+++ client/src/common/localStorage.ts
@@ -1,4 +1,5 @@
 const LS_NAME_KEY = "maze-player-name";
+const LS_ROOM_KEY = "maze-player-room";
 
 export function setPlayerName(name: string) {
   localStorage.setItem(LS_NAME_KEY, name);
@@ -6,4 +7,12 @@ export function setPlayerName(name: string) {
 
 export function getPlayerName() {
   return localStorage.getItem(LS_NAME_KEY);
+}
+
+export function setRoom(roomCode: string) {
+  localStorage.setItem(LS_ROOM_KEY, roomCode);
+}
+
+export function getRoom() {
+  return localStorage.getItem(LS_ROOM_KEY);
 }
--- client/src/pages/Lobby.tsx
+++ client/src/pages/Lobby.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router";
-import { setPlayerName } from "../common/localStorage";
+import { getPlayerName, getRoom, setPlayerName, setRoom } from "../common/localStorage";
 
 export function Lobby(): JSX.Element {
   const navigate = useNavigate();
@@ -13,7 +13,8 @@ export function Lobby(): JSX.Element {
     const playerName = String(formData.get("name"));
     setPlayerName(playerName);
 
-    const roomCode = formData.get("room");
+    const roomCode = formData.get("room") as string;
+    setRoom(roomCode);
     navigate(`/room/${roomCode}`);
   };
   return (
@@ -21,11 +22,11 @@ export function Lobby(): JSX.Element {
       <form onSubmit={handleSubmit} className="form-control">
         <label className="input-group">
           <span>Name</span>
-          <input name="name" type="text" required={true} placeholder="Your character name" className="input input-bordered" autoComplete="off" />
+          <input name="name" type="text" required={true} placeholder="Your character name" className="input input-bordered" autoComplete="off" defaultValue={getPlayerName() ?? ""}/>
         </label>
         <label className="input-group mt-2">
           <span>Room</span>
-          <input name="room" type="text" required={true} placeholder="The room code" className="input input-bordered" autoComplete="off" />
+          <input name="room" type="text" required={true} placeholder="The room code" className="input input-bordered" autoComplete="off" defaultValue={getRoom() ?? ""} />
         </label>
         <label className="label cursor-pointer">
           <span className="label-text">Join room</span> 
