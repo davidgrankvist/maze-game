d43627721e3a629ef03eb0a8d80190809e72bead
David Grankvist <grankvist.david@gmail.com>
2022-10-26 23:57:57 +0200

    use hero for room page and add invite action

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -19,38 +19,47 @@ export function Room(): JSX.Element {
   ]);
   const [isHost] = useState(true);
 
+  const onInvite = async () => {
+    await navigator.clipboard.writeText(window.location.href);
+  }
   const onStartGame = () => {
     // TODO call some api here to create the room
 
     navigate(`/game/${roomCode}`);
   };
   return (
-    <div className="w-full h-1/2 flex justify-center">
-      <div className="w-1/3 mt-20 flex flex-col">
-        {isHost ? (
-          <div className="flex flex-col items-center">
-            <p className="mb-3">You are the host. You have power.</p>
-            <button className="w-1/3 btn btn-primary" onClick={onStartGame}>Start game</button> 
-          </div>
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center mb-60">
+        <div className="max-w-md">
+          {isHost ? (
+            <>
+              <h1 className="text-5xl font-bold">You are the host</h1>
+              <p className="py-6">Wait for everyone to join and then start the game!</p>
+              <div className="w-full place-content-evenly">
+                <button className="btn btn-secondary lg:mr-5" onClick={onInvite}>Copy invite link</button>
+                <button className="btn btn-primary" onClick={onStartGame}>Start game</button>
+              </div>
+            </>
           ) : (
-          <div className="flex flex-col items-center">
-            <p>Waiting for the host to start the game</p>
-            <progress className="w-3/4 progress progress-info"></progress>
+            <>
+              <h1 className="text-5xl font-bold">Game starting soon</h1>
+              <p className="py-6">Waiting for the host to start the game</p>
+              <progress className="w-3/4 progress progress-info"></progress>
+            </>
+          )}
+          <div className="absolute left-0 w-full flex justify-center">
+          <table className="mt-5 w-96 table table-zebra">
+            <thead>
+              <tr>
+                <th className="bg-base-300">Players on this room</th>
+              </tr>
+            </thead>
+            <tbody>
+              {players.map(pl => <tr key={pl.name}><td>{pl.name === playerName ? pl.name + " (you)" : pl.name}</td></tr>)}
+            </tbody>
+          </table>
           </div>
-        )}
-      </div>
-     <div className="divider divider-horizontal" />
-     <div className="w-1/3 mt-20 mb-20">
-        <table className="w-full table table-zebra">
-          <thead>
-            <tr>
-              <th>Players on this room</th>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map(pl => <tr key={pl.name}><td>{pl.name === playerName ? pl.name + " (you)" : pl.name}</td></tr>)}
-          </tbody>
-        </table>
+        </div>
       </div>
     </div>
   );
